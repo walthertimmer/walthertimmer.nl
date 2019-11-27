36e48a2f2274aa98f9ef7a57af78ddb4c653b417
walthertimmer <31596984+walthertimmer@users.noreply.github.com>
2019-11-27 23:51:30 +0100

    basic site werkend

--- walthertimmer-django/hello/views.py
+++ walthertimmer-django/hello/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import re 
+from datetime import datetime
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, "hello/home.html")
+
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
+
+
+
+def hello_there(request, name):
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
+
+    #now = datetime.now()
+    #formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+# Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return HttpResponse(content)
+
+
