831c8a37c9330b71a6e61ea1328173add642544f
walthertimmer <31596984+walthertimmer@users.noreply.github.com>
2020-02-28 11:38:56 +0100

    remove django

--- walthertimmer-django/hello/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from hello import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("hello/<name>", views.hello_there, name="hello_there"),
-    path("about/", views.about, name="about"),
-    path("contact/", views.contact, name="contact"),
-]
