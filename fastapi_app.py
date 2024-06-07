2c5c96dd9f547d432a4b95517866ada75a33103a
Walther D. Timmer <31596984+walthertimmer@users.noreply.github.com>
2024-06-07 16:54:48 +0200

    fix for frontmatter?

--- fastapi_app.py
+++ fastapi_app.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
 from fastapi.requests import Request
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 import markdown
 import os
 import frontmatter
 
-
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+@app.get("/favicon.ico")
+async def favicon():
+    return RedirectResponse(url="/static/favicon.ico")
 
 templates = Jinja2Templates(directory="templates")
 
@@ -18,8 +20,6 @@ def read_md_file(file_path):
     with open(file_path, 'r') as f:
         post = frontmatter.load(f)
     return post
-
-
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
